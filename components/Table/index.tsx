28f501453e03214067aaa45d80ee5f69c330b4b1
sht02048 <sht02048@gmail.com>
2025-01-30 22:46:30 +0900

    ♻️ Component code 이관

--- components/Table/index.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "./Header";
-
-export default function Table() {
-  return (
-    <div className="bg-[#101011] rounded-md h-screen border-[0.5px]">
-      <Header />
-    </div>
-  );
-}
